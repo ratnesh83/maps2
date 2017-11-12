fd6c09b84a9edc267754cf8566570950f976070e
Sumit Samanta <sumit.samanta@mail.click-labs.com>
2017-11-12 17:38:51 +0530

    sprint 2 change profile picture

--- src/app/theme/components/baPageTop/baPageTop.component.ts
+++ src/app/theme/components/baPageTop/baPageTop.component.ts
@@ -1,34 +1,56 @@
 import { Component } from '@angular/core';
 import { GlobalState } from '../../../global.state';
 import 'style-loader!./baPageTop.scss';
-import { AuthService } from '../../../auth/service/auth-service/auth.service';
+import { DataService } from '../../../services/data-service/data.service';
 import { Store } from '@ngrx/store';
 import * as auth from '../../../auth/state/auth.actions';
-import * as pass from '../../../pages/change-password/state/change-password-modal.action';
+import { JwtHelper } from 'angular2-jwt';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
-import { ChangePasswordModal } from '../../../pages/change-password/component/change-password-modal.component';
 
 @Component({
     selector: 'ba-page-top',
     templateUrl: './baPageTop.html',
 })
 export class BaPageTop {
-    
+
     public isScrolled: boolean;
     public isMenuCollapsed: boolean = false;
+    public jwtHelper: JwtHelper = new JwtHelper();
+    public user;
+    public storeData;
 
     constructor(private _state: GlobalState,
         private store: Store<any>,
         private router: Router,
-        private authService: AuthService,
-        private modalService: NgbModal) {
+        private dataService: DataService) {
+
         this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
             this.isMenuCollapsed = isCollapsed;
         });
 
+        this.storeData = this.store
+            .select('auth')
+            .subscribe((res: any) => {
+                if (res) {
 
+                }
+            });
+    }
 
+    ngOnInit() {
+        let token = localStorage.getItem('tokenSession');
+        if (token && !this.jwtHelper.isTokenExpired(token)) {
+            this.user = this.jwtHelper.decodeToken(token);
+            // this.store.dispatch({ type: auth.actionTypes.AUTH_GET_USER_DETAILS, payload: { userId: this.user._id } });
+        }
+
+    }
+
+    ngOnDestroy() {
+        if (this.storeData) {
+            //this.storeData.unsubscribe();
+        }
     }
 
     public toggleMenu() {
@@ -38,32 +60,13 @@ export class BaPageTop {
     }
 
     public scrolledChanged(isScrolled) {
-        if(this.isScrolled != undefined) { 
+        if (this.isScrolled != undefined) {
             this.isScrolled = isScrolled;
         }
     }
+
     logout() {
         this.store.dispatch(new auth.AuthLogoutAction());
-        // console.log("looged out")
-        // this.UserService.logoutUser().subscribe((result) => {
-        //     console.log(result);
-        //     if(result.message='success'){
-        //       //clear localstorage
-        //       this.authService.logout();
-        //       this.router.navigate(['login']);
-        //     }
-        //   }
-        //   , (error) => {
-        //     console.log(error)
-        //   }
-        // );
     }
-    changePassword() {
-        // this.store.dispatch()
 
-        const changePasswordActionModal = this.modalService.open(ChangePasswordModal, { size: 'lg' });
-        changePasswordActionModal.componentInstance.modalHeader = 'Large Modal';
-
-
-    }
 }
