02454498f4eeb0e2b289f4d56ebbb6c2a4e88ef6
Sumit Samanta <sumit.samanta@mail.click-labs.com>
2017-11-14 10:45:22 +0530

    sprint 2 create post a job form

--- src/app/pages/posts/components/post-job/post-job.component.ts
+++ src/app/pages/posts/components/post-job/post-job.component.ts
@@ -4,6 +4,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
 import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { EmailValidator, EqualPasswordsValidator, NameValidator } from '../../../theme/validators';
 import * as post from '../../state/post.actions';
 import * as app from '../../../../state/app.actions';
 import { BaThemeSpinner } from '../../../../theme/services';
@@ -20,52 +21,54 @@ import 'style-loader!./post-job.scss';
 
 export class PostJob implements OnInit {
 
-    public posts;
-    public page = 1;
-    public limit;
-    public pageIndex;
-    public count: number;
-    public name: string;
-    public role: string;
-    public value: 'all';
-    public filter;
-    public showPhone: boolean = false;
-    public showEmail: boolean = false;
+    @ViewChild('inputJobTitle') public _jobDetail: ElementRef;
+    public form: FormGroup;
+    public jobDetail: AbstractControl;
+    public category: AbstractControl;
+    public subCategory: AbstractControl;
+    public locationAddress: AbstractControl;
+    public startDate: AbstractControl;
+    public endDate: AbstractControl;
+    public jobDetails: AbstractControl;
+    public rateType: AbstractControl;
+    public jobRate: AbstractControl;
+    public labourCount: AbstractControl;
     public searchLocation;
-    public positions = [];
-    public markers = [];
-    public info;
     public postStore;
-    public selectedCategory;
     public categories = [];
-    public center;
+    public subCategories = [];
+    public rateTypes = [];
 
-    constructor(
+    constructor(private fb: FormBuilder,
         private store: Store<any>,
         private modalService: NgbModal,
         private router: Router,
         private toastrService: ToastrService
     ) {
-        this.center = '30.71889493430725, 76.81024353951216';
 
-        if (navigator.geolocation) {
-            let self = this;
-            navigator.geolocation.getCurrentPosition((response) => {
-                self.showPosition(response, self);
-            });
-        }
+        this.form = fb.group({
+            'jobDetail': ['', Validators.compose([Validators.required])],
+            'category': ['', Validators.compose([Validators.required])],
+            'subCategory': ['', Validators.compose([Validators.required])],
+            'locationAddress': ['', Validators.compose([Validators.required])],
+            'startDate': ['', Validators.compose([Validators.required])],
+            'endDate': ['', Validators.compose([Validators.required])],
+            'jobDetails': [''],
+            'rateType': ['', Validators.compose([Validators.required])],
+            'jobRate': [''],
+            'labourCount': ['', Validators.compose([Validators.required])]
+        });
 
-        this.info = {
-            employerName: null,
-            employerEmail: null,
-            employerPhoneNumber: null,
-            isPhoneNumberHidden: false,
-            profilePicture: null,
-            distance: 0,
-            address: null,
-            rate: 0,
-            requiredLabourers: 0,
-        };
+        this.jobDetail = this.form.controls['jobDetail'];
+        this.category = this.form.controls['category'];
+        this.subCategory = this.form.controls['subCategory'];
+        this.locationAddress = this.form.controls['locationAddress'];
+        this.startDate = this.form.controls['startDate'];
+        this.endDate = this.form.controls['endDate'];
+        this.jobDetails = this.form.controls['jobDetails'];
+        this.rateType = this.form.controls['rateType'];
+        this.jobRate = this.form.controls['jobRate'];
+        this.labourCount = this.form.controls['labourCount'];
 
         this.categories = [
             'Health Care',
@@ -73,95 +76,22 @@ export class PostJob implements OnInit {
             'Engineering'
         ];
 
-        // this.selectedCategory = this.categories[0];
-
         this.postStore = this.store
             .select('post')
             .subscribe((res: any) => {
                 if (res) {
-                    this.count = res.count;
-                    this.posts = [];
-                    if (res.posts) {
-
-                        for (let i = 0; i < res.posts.length; i++) {
-                            let coordinates = [0, 0];
-                            if (res.posts[i].employerAddress && res.posts[i].employerAddress.location) {
-                                coordinates = [res.posts[i].employerAddress.location.coordinates[1], res.posts[i].employerAddress.location.coordinates[0]];
-                            }
-                            let post = {
-                                id: res.posts[i]._id,
-                                employerName: res.posts[i].employerId ? res.posts[i].employerId.firstName + ' ' + res.posts[i].employerId.lastName : null,
-                                employerEmail: res.posts[i].employerId ? res.posts[i].employerId.email : null,
-                                employerPhoneNumber: res.posts[i].employerId ? res.posts[i].employerId.countryCode + res.posts[i].employerId.phoneNumber : null,
-                                isPhoneNumberHidden: res.posts[i].employerId ? res.posts[i].employerId.isPhoneNumberHidden : false,
-                                profilePicture: res.posts[i].employerId ? res.posts[i].employerId.profilePicture ? res.posts[i].employerId.profilePicture.original : '' : null,
-                                coordinates: coordinates,
-                                distance: res.posts[i].distance,
-                                rate: res.posts[i].rate,
-                                rateType: res.posts[i].rateType,
-                                title: res.posts[i].title,
-                                address: res.posts[i].employerAddress ? res.posts[i].employerAddress.addressLine1 + ', ' + res.posts[i].employerAddress.city : ''
-                            };
-                            this.posts.push(post);
-                        }
-                    }
+                    
                 }
             });
     };
 
     ngOnInit() {
-        this.getAllPosts();
-    }
 
-    showPosition(position, self) {
-        if (position && position.coords) {
-            // let latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            let latlng = new google.maps.LatLng(30.71889493430725, 76.81024353951216);
-            let geocoder = new google.maps.Geocoder();
-            geocoder.geocode({ 'location': latlng }, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[0]) {
-                        // self.searchLocation = results[0].formatted_address;
-                    }
-                }
-            });
-        }
     }
 
     ngOnDestroy() {
         if (this.postStore) {
             this.postStore.unsubscribe();
-        }
-    }
-
-    postJob() {
-        // console.log('Job posted');
-    }
-
-    getAllPosts() {
-        this.store.dispatch({
-            type: post.actionTypes.APP_GETALL_JOB, payload: {
-                currentPage: this.page,
-                limit: 10,
-                role: this.role,
-                filter: this.filter,
-                value: this.value
-            }
-        });
-    };
-
-    showPostDetail(data) {
-        //localStorage.setItem('viewJobId', data.id);
-        //this.router.navigate(['pages/users/viewpost']);
-    }
-
-    changeMap(lat, lng) {
-        this.center = lat + ', ' + lng;
-    }
-
-    changeCategory(event, data) {
-        if (event && event.isUserInput) {
-            // console.log(data);
         }
     }
 
@@ -196,36 +126,16 @@ export class PostJob implements OnInit {
             }
         }
         this.searchLocation = formattedAddress;
-        this.changeMap(latitude, longitude);
     }
 
-    showMarkerInfo({ target: marker }, data) {
-        if (data) {
-            this.info = {
-                id: data.id,
-                employerName: data.employerName,
-                employerEmail: data.employerEmail,
-                employerPhoneNumber: data.employerPhoneNumber,
-                isPhoneNumberHidden: data.isPhoneNumberHidden,
-                profilePicture: data.profilePicture,
-                distance: data.distance ? data.distance.toFixed(1) : data.distance,
-                address: data.address,
-                rate: data.rate,
-                rateType: data.rateType,
-                title: data.title
-            };
+    changeCategory(event, data) {
+        if (event && event.isUserInput) {
+            // console.log(data);
         }
-        this.showPhone = false;
-        this.showEmail = false;
-        marker.nguiMapComponent.openInfoWindow('iw', marker);
     }
 
-    showPhoneInfo() {
-        this.showPhone = true;
-    }
-
-    showEmailInfo() {
-        this.showEmail = true;
+    postJob() {
+        console.log(this.form.value);
     }
 
 }
